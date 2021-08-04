20941445317a6da9a5217fa858f8280c16600b99
brlaney42 <brlaney@outlook.com>
2021-08-04 18:24:24 -0500

    Fixed double capital letters

--- src/components/en/Deposits.tsx
+++ src/components/en/Deposits.tsx
@@ -10,12 +10,12 @@ function preventDefault(event: React.MouseEvent) {
 export default function Deposits() {
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
+      <Title>Recent deposits</Title>
       <Typography component='p' variant='h4'>
         $3,024.00
       </Typography>
       <Typography color='text.secondary' sx={{ flex: 1 }}>
-        on 15 March, 2019
+        on March 15, 2019
       </Typography>
       <div>
         <Link color='primary' href='#' onClick={preventDefault}>
